4d243aff346fa012a799de06c18d515613d7b60a
Raimundo Feitosa <ray.feitosa97@gmail.com>
2023-10-09 08:08:44 -0400

    Funções sem nome

---
+++ exerciciosJs/funcao/funcaoAnonima.js
@@ -0,0 +1,12 @@
+const soma = function(x, y) {
+    return x + y;
+}
+
+const imprimirResultado = function(a, b, operacao = soma) {
+    console.log(operacao(a, b));
+}
+
+imprimirResultado(3, 4);
+imprimirResultado(9, 9, soma);
+imprimirResultado(1, 5, function(a, b) { return a - b});
+imprimirResultado(4, 8, (a, b) => a * b);
