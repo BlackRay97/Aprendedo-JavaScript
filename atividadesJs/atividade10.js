2996884eb3c5490f20617edfd22e3ba4a4b863c4
Raimundo Feitosa <ray.feitosa97@gmail.com>
2023-10-16 19:45:39 -0400

    atividade 10 resolvida - numeros divisiveis por 3

---
+++ atividadesJs/atividade10.js
@@ -0,0 +1,8 @@
+function DivisivelPor3(inteiro) {
+    return inteiro % 3 == 0 ? true : false;
+}
+
+console.log(DivisivelPor3(3));
+console.log(DivisivelPor3(7));
+console.log(DivisivelPor3(1));
+console.log(DivisivelPor3(9));
