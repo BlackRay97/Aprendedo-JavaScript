3c2c245b0aef85c27104757733eab2354565e12d
Raimundo Feitosa <ray.feitosa97@gmail.com>
2023-11-02 08:05:05 -0400

    atividade 34 resolvida - Adicionando um vetor a outro

---
+++ atividadesJs/atividade34.js
@@ -0,0 +1,9 @@
+const vetorPilha = [1, 2, 3, 4];
+const vetorAdicional = [5, 6, 7, 8];
+
+vetorAdicional.forEach(e => {
+    vetorPilha.push(e);
+});
+
+console.log(vetorPilha);
+console.log(vetorAdicional);
